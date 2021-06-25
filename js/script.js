a92a33ef508e15459659ecbb9ca1d39d6265833a
mitri-dvp <mitri.dvp@gmail.com>
2021-06-25 16:37:42 -0300

    Version Final de Diseño Web de Página One Page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 // Elements
 const burger = document.querySelector('.burger')
 const mobile_nav = document.querySelector('.mobile-nav')
-// const modal_exit = document.querySelector('.modal-exit')
 
 // Listeners
 burger.addEventListener('click', () => {
   mobile_nav.classList.toggle('hidden')
 })
-// modal_exit.addEventListener('click', (e) => {
-//   e.target.closest('.modal-container').remove()
-// })
