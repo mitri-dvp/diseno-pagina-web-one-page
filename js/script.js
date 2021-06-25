c2bad910004ea0e2068d4d27230d3cb3797ac964
mitri-dvp <mitri.dvp@gmail.com>
2021-06-25 17:01:30 -0300

    JavaScript eliminado, uso de CSS para menu responsivo

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Elements
-const burger = document.querySelector('.burger')
-const mobile_nav = document.querySelector('.mobile-nav')
-
-// Listeners
-burger.addEventListener('click', () => {
-  mobile_nav.classList.toggle('hidden')
-})
